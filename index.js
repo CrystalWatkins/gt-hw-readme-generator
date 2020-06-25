5df804fa66e14b881d731d4a7b5b7ca1bc82c2dc
Crystal <crystalwatkins428@gmail.com>
2020-06-25 17:45:27 -0400

    update array of questions

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer
+.prompt([
+    {
+        type: "input",
+        message: "What is your gitHub username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What if your Project's Title?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Brief Description of Your Project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Instructions for Installation",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What is the Usage for this project?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Did anyone Contribute to your project?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Were any tests needed for this project?",
+        name: "tests",
+    },
+    {
+        type: "checkbox",
+        message: "What license do you want to use?",
+        choices: ["MIT License", "The Unlicense", "Random", "None"],
+        name: "license",
+    },  
+])
+
+.then(function(response) {
+    console.log(response);
+})
+
 // array of questions for user
 const questions = [
-
+    
+    //https://www.npm.com/package/inquirer#questions
 ];
 
 // function to write README file
@@ -9,8 +67,15 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+// use the inquirer package
 }
 
 // function call to initialize program
 init();
+
+fs.writeFile("./generatedREADME.md", "This is a readme file", function(err) {
+    if(err) {
+        throw err;
+    }
+    console.log("Wrote the file");
+});
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,43 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description 
+  
+  ${data.description}
+
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+ 
+  ${data.installation}  
+
+  ## Usage 
+ 
+  ${data.usage}
+  
+  ## License
+  
+  License choice: ${data.license} 
+  
+  ## Contributing
+
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+
+  For any additional questions, please reach out to me at my 
+  email account: ${data.email}. To see the additional work
+  that I have created, please visit me at ${data.username}.
 `;
 }
 
